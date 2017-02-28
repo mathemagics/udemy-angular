d579bc7c9f42263d29674dc8d54fd6affe3c2d4b
Paul WB <higgsbison@gmail.com>
2017-02-27 16:04:51 -0800

    use  service

--- Code-Modules-Apps-Controllers/app.js
+++ Code-Modules-Apps-Controllers/app.js
@@ -1,7 +1,14 @@
+
 // MODULE
 var angularApp = angular.module('myApp', []);
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', function ($scope) {
-
+angularApp.controller('mainController', [
+  '$scope',
+  '$timeout',
+  function ($scope, $timeout) {
+    $scope.name= 'Paul';
+    $timeout(function() {
+       $scope.name = 'Everyone';
+    }, 3000)
 }]);
